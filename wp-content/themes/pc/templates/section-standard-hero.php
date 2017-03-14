0c4632537d26549aa3b1d2d173d48cc7eacec45b
Tyler Morian <tyemorian@gmail.com>
2017-03-13 21:02:03 -0700

    Updated wordpress and resolved broken function call in hero section

--- wp-content/themes/pc/templates/section-standard-hero.php
+++ wp-content/themes/pc/templates/section-standard-hero.php
@@ -1,8 +1,9 @@
 <section class="hero">
 	<?php site_background_image($background); ?>
 	<div class="content-container pinned-bottom">
-		<?php if (!empty(get_field('hero_title'))): ?>
-			<h1 class="title"><?php the_field('hero_title'); ?></h1>
+		<?php $title = get_field('hero_title'); ?>
+		<?php if (!$title): ?>
+			<h1 class="title"><?php echo $title; ?></h1>
 		<?php else: ?>
 			<h1 class="title"><?php the_title(); ?></h1>
 		<?php endif ?>
